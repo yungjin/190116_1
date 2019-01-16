cef625c51ccabc4b34f50ab643d4a193f73d5422
영진 최 <chyj0318@naver.com>
2019-01-16 13:03:16 +0900

    커밋

---
+++ Solution1/ClassLibrary1/DataBase.cs
@@ -0,0 +1,46 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class DataBase
+    {
+        //private string strConnection1 = string.Format("server={0};user={1};password={2};database={3};", "192.168.3.139", "root", "test", "test");
+        //private string strConnection2 = string.Format("server={0};user={1};password={2};database={3};", "192.168.3.154", "root", "1234", "gudi");
+
+        public MySqlConnection GetConnection()
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection();
+
+                string path = "\\public\\DBInfo.json";
+                string result = new StreamReader(File.OpenRead(path)).ReadToEnd();
+                JObject jo = JsonConvert.DeserializeObject<JObject>(result);
+                Hashtable map = new Hashtable();
+                foreach (JProperty col in jo.Properties())
+                {
+                    Console.WriteLine("{0} : {1}", col.Name, col.Value);
+                    map.Add(col.Name, col.Value);
+                }
+
+                string strConnection1 = string.Format("server={0};user={1};password={2};database={3};",map["server"], map["user"], map["password"], map["database"]);
+                conn.ConnectionString = strConnection1;
+                conn.Open();
+
+                return conn;
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Solution1/WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            Class1 c1 = new Class1();
+            Console.WriteLine(c1.GetInt());
+            
+            DataBase db = new DataBase();
+            MySqlConnection conn = db.GetConnection();
+            if (conn == null)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                Console.WriteLine("Success!");
+            }
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
